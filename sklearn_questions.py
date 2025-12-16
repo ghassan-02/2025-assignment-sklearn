26b02393d169fdf026bfed84e1ec8c6426a36420
Ghassan Ahwach <ghassan.ahwash@hotmail.com>
2025-12-16 16:38:50 +0100

    sklearn commit

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -226,6 +226,7 @@ class MonthlySplit(BaseCrossValidator):
             ).unique().sort_values()
         
         return max(int(len(months) - 1), 0)
+    
     def split(self, X, y, groups=None):
         """Generate indices to split data into training and test set.
 
